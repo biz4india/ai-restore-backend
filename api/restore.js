d6c1f36d004eabd74bdc56e09968502cec88a41c
biz4india <biz4india@gmail.com>
2026-02-18 17:54:50 +0530

    Create restore.js

---
+++ api/restore.js
@@ -0,0 +1,32 @@
+import Replicate from "replicate";
+
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+
+    const replicate = new Replicate({
+      auth: process.env.REPLICATE_API_TOKEN,
+    });
+
+    const { image } = req.body;
+
+    const output = await replicate.run(
+      "sczhou/codeformer:latest",
+      {
+        input: {
+          image: image,
+          fidelity: 0.7
+        }
+      }
+    );
+
+    return res.status(200).json({ result: output[0] });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
